d166799809c6055e530c6b24c4ff83ca346df5d6
anurag56964 <anurag.case@gmail.com>
2023-09-29 12:20:56 -0400

    serverWorking

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const HTTP_PORT = process.env.PORT || 8080; // assign a port
 const path = require('path');
 
 // start the server on the port and output a confirmation ot the console
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+// app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () => {
+    console.log(`Express http server listening on port ${HTTP_PORT}`);
 
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.redirect(path.join(__dirname, '/views/about.html'));
+    res.redirect('views/about.html');
   });
+
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/about.html'));
+  })
+
+
+});
 
 
 
